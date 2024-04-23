7c4ae391aec9acb0703dd0d08b117c6b75e22663
EnricoClem <enrico.clem94@gmail.com>
2024-04-23 16:54:56 +0200

    ADD - header interactive navigation menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,34 +5,95 @@ export default {
             navBarItems: [
                 {
                     'name': 'HOME',
+                    'link': '#',
                     subMenu: [
-                        'HOME-1', 'HOME-2', 'HOME-3', 'HOME-4'
+                        {
+                            'subName': 'HOME-1',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'HOME-2',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'HOME-3',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'HOME-4',
+                            'subLink': '#',
+                        }, 
                     ]
                 }, {
                     'name': 'ABOUT',
+                    'link': '#',
                     subMenu: [
-                        'ABOUT-1', 'ABOUT-2'
+                        {
+                            'subName': 'ABOUT-1',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'ABOUT-2',
+                            'subLink': '#',
+                        }, 
                     ]
                 }, {
                     'name': 'SERVICE',
+                    'link': '#',
                     subMenu: []
                 }, {
                     'name': 'PORTFOLIO',
+                    'link': '#',
                     subMenu: [
-                        'PORTFOLIO-1', 'PORTFOLIO-2', 'PORTFOLIO-3'
+                        {
+                            'subName': 'PORTFOLIO-1',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'PORTFOLIO-2',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'PORTFOLIO-3',
+                            'subLink': '#',
+                        }, 
                     ]
                 }, {
                     'name': 'SHOP',
+                    'link': '#',
                     subMenu: [
-                        'SHOP-1', 'SHOP-2', 'SHOP-3', 'SHOP-4', 'SHOP-5'
+                        {
+                            'subName': 'SHOP-1',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'SHOP-2',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'SHOP-3',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'SHOP-4',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'SHOP-5',
+                            'subLink': '#',
+                        }, 
                     ]
                 }, {
                     'name': 'BLOG',
+                    'link': '#',
                     subMenu: [
-                        'BLOG-1', 'BLOG-2', 'BLOG-3'
+                        {
+                            'subName': 'BLOG-1',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'BLOG-2',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'BLOG-3',
+                            'subLink': '#',
+                        }, {
+                            'subName': 'BLOG-4',
+                            'subLink': '#',
+                        }, 
                     ]
                 }, {
                     'name': 'CONTACT',
+                    'link': '#',
                     subMenu: []
                 }, 
             ]
@@ -43,9 +104,21 @@ export default {
 
 <template>
 
-<li v-for="navItem in navBarItems" class="flex">
-    <h3>{{ navItem.name }}</h3>
-    <div class="arrow-menu" v-if="navItem.subMenu.length !== 0"><font-awesome-icon :icon="['fas', 'angle-down']" /></div>
+<!-- bottone principale -->
+<li v-for="navItem in navBarItems" class="flex nav-button">
+    <a :href="`${navItem.link}`">{{ navItem.name }}</a>
+
+    <!-- bottone menù a tendina -->
+    <div class="arrow-menu" v-if="navItem.subMenu.length !== 0">
+        <font-awesome-icon :icon="['fas', 'angle-down']" />
+        <ul class="sub-menu flex column gap-md">
+            <li v-for="subNavItem in navItem.subMenu">
+                <a :href="`${subNavItem.subLink}`">{{ subNavItem.subName }}</a>
+            </li>
+        </ul>
+    </div>
+    <span v-else></span>
+
 </li>
 
 </template>
@@ -53,9 +126,21 @@ export default {
 <style lang="scss" scoped>
 @use '../style/partials/variables.scss';
 
-h3 {
+.nav-button {
+    position: relative;
+
+    &:hover .sub-menu {
+        display: flex;
+    }
+}
+
+a {
     font-weight: 500;
     font-size: small;
+
+    &:hover {
+        color: variables.$brand-yellow;
+    }
 }
 
 .arrow-menu {
@@ -63,6 +148,22 @@ h3 {
     padding-left: 5px;
     font-size: 12px;
     margin-top: auto;
+}
+
+.sub-menu {
+    display: none;
+    color: black;
+    background-color: variables.$white;
+    position: absolute;
+    left: 0;
+    top: 42.5px;
+    width: 150px;
+    box-shadow: 0px 0px 20px -10px #888888;
+    padding: 20px 0;
+}
+
+.sub-menu a {
+    padding: 15px 20px;
 }
 
 </style>
