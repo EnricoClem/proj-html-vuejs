e71b2c97cd7ad59d5868826cce239469ad94ab80
EnricoClem <enrico.clem94@gmail.com>
2024-04-23 10:45:22 +0200

    ADD - header, main and footer as vue components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+
   export default {
-    
+    components: {
+      Header,
+      Main,
+      Footer
+    }
   };
 </script>
 
 <template>
-  <div>
 
-  </div>
+  <Header />
+
+  <Main />
+
+  <Footer />
+
 </template>
 
 <style lang="scss">
+
+@use './style/general';
 
 </style>
