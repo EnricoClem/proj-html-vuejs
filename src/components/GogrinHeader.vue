0074884c1da05af85c6dcb04663363d72579a315
EnricoClem <enrico.clem94@gmail.com>
2024-04-23 12:31:21 +0200

    FIX - header container order and css rules

--- src/components/GogrinHeader.vue
+++ src/components/GogrinHeader.vue
@@ -6,29 +6,30 @@
 
 <template>
     <header>
-        <div class="container-lg">
-        <div class="head-info flex between">
-            <div class="flex paragraph gap-sm">
-                <div>
-                    <font-awesome-icon :icon="['fas', 'location-dot']" /> North bauthula, tala, USA
+        <!-- Primo blocco HEADER con info di contatto -->
+        <div class="container-lg head-info">
+            <div class="container-md flex between">
+                <div class="flex paragraph gap-sm">
+                    <div>
+                        <font-awesome-icon :icon="['fas', 'location-dot']" /> North bauthula, tala, USA
+                    </div>
+                    <div>
+                        <font-awesome-icon :icon="['fas', 'envelope']" /> demo@example.com
+                    </div>
                 </div>
-                <div>
-                    <font-awesome-icon :icon="['fas', 'envelope']" /> demo@example.com
+                <div class="flex paragraph gap-md">
+                    <div>
+                        <font-awesome-icon :icon="['fas', 'clock']" /> 8:30AM-8:30PM
+                    </div>
+                    <ul class="flex gap-sm">
+                        <li><font-awesome-icon :icon="['fab', 'facebook-f']" /></li>
+                        <li><font-awesome-icon :icon="['fab', 'twitter']" /></li>
+                        <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
+                        <li><font-awesome-icon :icon="['fab', 'pinterest']" /></li>
+                    </ul>
                 </div>
-            </div>
-            <div class="flex paragraph gap-md">
-                <div>
-                    <font-awesome-icon :icon="['fas', 'clock']" /> 8:30AM-8:30PM
-                </div>
-                <ul class="flex gap-sm">
-                    <li><font-awesome-icon :icon="['fab', 'facebook-f']" /></li>
-                    <li><font-awesome-icon :icon="['fab', 'twitter']" /></li>
-                    <li><font-awesome-icon :icon="['fab', 'instagram']" /></li>
-                    <li><font-awesome-icon :icon="['fab', 'pinterest']" /></li>
-                </ul>
             </div>
         </div>
-    </div>
     </header>
 </template>
 
@@ -38,7 +39,7 @@
 .head-info {
     background-color: variables.$brand-green;
     color: white;
-    padding: 10px 100px;
+    padding: 10px;
 }
 
 </style>
